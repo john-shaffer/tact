e195038a6a262c07b8003e64b754b459efe37132
John Shaffer <jdsha@pm.me>
2026-06-10 10:18:13 -0500

    Update deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "tact scenario runner";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     clj-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:jlesquembre/clj-nix";
